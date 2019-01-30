7fe11a249d802d2b8944354da7d607263acfebc7
Cuong Do <cuong.do@acaziasoft.com>
2019-01-30 23:14:28 +0700

    Update cdn

--- application/views/admin_main_layout.php
+++ application/views/admin_main_layout.php
@@ -184,10 +184,7 @@
 </script>
 
 <!-- jQuery  -->
-<script
-		src="https://code.jquery.com/jquery-2.2.4.min.js"
-		integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-		crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/detect.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/fastclick.js'); ?>"></script>
