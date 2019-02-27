903b659392286979be6753c071653052bf8b2efb
Cuong Do <cuong.do@acaziasoft.com>
2019-02-27 23:53:53 +0700

    Update privacy policy

--- application/views/sites/homepage/privacy-policy.php
+++ application/views/sites/homepage/privacy-policy.php
@@ -18,12 +18,46 @@
             <img src="<?= base_url('sites/homepage/images/logo.png') ?>" alt="10-block">
           </a>
           <div class="block">
-            <h2>Privacy Policy</h2>
-            <p>Last updated June 12, 2018</p>
-            <p>Welcome to the service provided by Second Screen, Inc. (“Second Screen”, “we”, “us”, and/or “our”). This Privacy Policy has been created to provide information about our company, our website, and the services we provide (including through our mobile applications), (collectively the “Services”) to Users (’you” and/or “your”). This Privacy Policy sets forth Second Screen’s policy with respect to information, including personally identifiable data (“Personal Data”) that is collected from Users of the Services.</p>
-            <p>Information We Collect:</p>
-            <p>When you interact with us through the Services, we may collect Personal Data and other information from you, as further described below:</p>
-            <p class="last">Personal Data That You Provide Through The Services: We collect Personal Data from you when you voluntarily provide information, such as when you register for access to our Services and/or third party services (for example, username and email address provided by services such as Facebook Connect), provide us with credit card and other payment withdrawal of funds, contact us with inquiries or respond to one of our surveys. Wherever Second Screen collects Personal Data we make an effort to provide a link to this Privacy Policy. (edited) </p>
+            <h2>Terms of Use and Privacy Policy</h2>
+            <p>Updated February 22, 2019</p>
+            <p>Welcome to the service provided by Second Screen, Inc. dba 10 Block (“10 Block”, “we”, “us”, and/or “our”). We provide a free ad-based and a paid subscription service (“Services”) that allows you to discover, watch, talk about, review and share 10 Block content streamed over the Internet to mobile devices and other connected devices, and includes all our functions and user interfaces, as well as licensed third-party content, and our software.</p>
+	          <p></p>
+	          <p>By using the 10 Block mobile or web apps, you have accepted these Terms of Use and our Privacy Policy.</p>
+	          <p></p>
+	          <p>Privacy Policy & Personal Data Collection<br/>This Privacy Policy has been created to provide information about our company, our mobile applications, our website and the Services to our users (“you” and/or “your”). This Privacy Policy sets forth 10 Block’s policy with respect to personally identifiable data (“Personal Data”) that is collected from users of the Service.</p>
+	          <p></p>
+	          <p>We collect Personal Data from you when you voluntarily provide such information, such as when you register for access to our Services and/or third party services (for example, username and email address produced by services such as Facebook Connect), provide us with credit card and other payment information, contact us with inquiries or respond to one of our surveys. Where ever 10 Block collects Personal Data, we make an effort to provide a link to this Privacy Policy.</p>
+	          <p></p>
+	          <p>Free Trial<br/>You can use 10 Block Service for free, for a limited time. When your time has expired you will see a pay wall in the app. At that point you can continue using the Service for free if you complete the quick community-building task (such as inviting a friend to join you in the app or sharing a story with your contacts) when you are prompted by the app. Free use of the Service is determined by 10 Block at its sole discretion and we may limit eligibility or duration to prevent free use abuse. We reserve the right to revoke the free trial and put your account on hold in the event that we determine that you are not eligible.</p>
+	          <p></p>
+	          <p>Subscription<br/>Your 10 Block monthly subscription automatically renews until terminated. To subscribe to the 10 Block service you need Internet access and a current, valid credit card (“Payment Method”). You must cancel your membership before it renews each month in order to avoid further billing. To view your monthly subscription details, see "Billing" in the app settings. You can cancel your 10 Block subscription at any time, and you will have full access to the Service until the end of your billing period. We do not provide refunds for partial month subscriptions or unwatched 10 Block content. To cancel your subscription or update your Payment Method, go to Billing in the app settings and follow the instructions. Check with your Payment Method service provider for any surcharges for transactions or processing your subscription. We reserve the right to change our subscription pricing or plan at any time and with our sole and absolute discretion.</p>
+	          <p></p>
+	          <p>10 Block Service<br/>You must be 17+ years old to use the Service. During your use of our Service, we grant you a limited, non-exclusive, non-transferable right to access and watch content, create a public profile, post comments and reviews, and create watch lists and a list of favorites, and for you to interact with, follow and be followed by other Service users. No further right, title or interest shall be transferred to you.</p>
+	          <p></p>
+	          <p>Ownership<br/>The person who created the 10 Block account is considered the “Account Owner”. As Account Owner, you are responsible for your public profile in the app, as well as maintaining the accuracy of the information you provide to us relating to your account. We can terminate your account or place your account on hold in order to protect you, us or our partners from identity theft or other fraudulent activity.</p>
+	          <p></p>
+	          <p>User Generated Content<br/>The Service allows for you to create a public profile, and using that profile comment on blocks and other user’s comments on blocks, and publish reviews in the app (“User Generated Content”). You are solely responsible for your own User Generated Content. You agree to abide by our Community Rules in your comments and reviews, and you agree not to upload any software viruses or any other computer code, files or programs to the Service in the form of User Generated Content. We may terminate or restrict your use of our service if you violate these Terms of Use or are engaged in abusive, predatory, illegal or fraudulent use of the Services.</p>
+	          <p></p>
+	          <p>You hereby grant, and you represent and warrant that you have the right to grant an irrevocable, nonexclusive, royalty-free and fully paid, worldwide license to 10 Block to reproduce, distribute, publicly display and perform, prepare derivative works of, incorporate into other works, and otherwise use your User Generated Content in our Services. You agree to irrevocably waive any claims and assertions of moral rights or attribution with respect to your User Generated Content.</p>
+	          <p></p>
+	          <p>Other Users<br/>You acknowledge and agree that we are not responsible for any other User Generated Content on the Services and we make no guarantees regarding the accuracy, currency, suitability, or quality of any User Generated Content, and we assume no responsibility for any User Generated Content. Your interactions with other Service users are solely between you and such a user. You agree that 10 Block will not be responsible for any loss or damage incurred as the result of any such interactions. If there is a dispute between you and any Service user, we are under no obligation to become involved.</p>
+	          <p></p>
+	          <p>Use the Report button in the app to report any User Generated Content or behavior on the Service that you think violates our terms. We will determine if it is in violation of our Terms of Use.</p>
+	          <p></p>
+	          <p>Community Rules<br/>10 Block does not permit violence (any content that we deem to be inciting, threatening or promoting, or planning violence or harm to yourself or others), hate speech (any attacks on other users for their race, ethnicity, nationality, religion, gender, sexual orientation or disability), pornography, copyright violations (share only content you’ve created or have the right to share) and spam (any actions which repeatedly infringe on the attention of other Services users without substance).</p>
+	          <p></p>
+	          <p>Some 10 Block content may be available for temporary download and offline viewing (“Offline Playback”) on supported devices and in certain territories. You agree not to archive, download, reproduce, distribute, modify, display, perform, publish, license, create derivative works from, offer for sale, or use content and information contained on or obtained from or through the Services.</p>
+	          <p></p>
+	          <p>Copyright Claims<br/>We respect copyright. All the content provided by our Services is fully licensed. Any claims of copyright infringement can be sent to copyright@get10block.com and a DMCA Notice Of Alleged Infringement can be mailed to 10 Block (Second Screen, Inc. 1448 Abbot Kinney Boulevard, Venice, CA 90291 Do not make false claims. Any misuse of this process may result in the suspension of your account or other legal consequences.</p>
+	          <p></p>
+	          <p>Disclaimers of Warranties and Limitations on Liability</p>
+	          <p>The 10 Block Service is provided “as is” and “as available”. We do not guarantee that our Services will be uninterrupted and error-free.</p>
+	          <p></p>
+	          <p>Governing Law<br/>These Terms of Use shall be governed by the laws of Delaware, U.S.</p>
+	          <p></p>
+	          <p>Changes To Terms of Use<br/>We reserve the right to change these Terms of Use at any time and to be effectively immediately.</p>
+	          <p></p>
+	          <p class="last">Communications<br/>We will communicate with you regarding your account by electronic means only. You agree these electronic messages will satisfy legal requirements for any communications.</p>
             <ul class="social-media">
               <li><a href="https://twitter.com/get10block/"><i class="fa fa-twitter"></i></a></li>
               <li><a href="https://www.instagram.com/get10block/"><i class="fa fa-instagram"></i></a></li>
