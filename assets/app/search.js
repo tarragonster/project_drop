1b130dc8a737d45b2b04b09f5f0127bdfc468fd1
DuyenNguyen <duyennth@acaziasoft.com>
2019-05-22 11:15:05 +0700

    Update search.js

--- assets/app/search.js
+++ assets/app/search.js
@@ -1,15 +1,28 @@
+var t = 0;
+var lastTime = 0;
 function searchUser()
 {
-	var query = $('#search_text').val();
+
+    // var date = new Date();
+    var time1 = ++t;
+    // console.log('timeSend: ' + time1);
+    var query = $('#search_text').val();
  	$.ajax({
         type: 'GET',
         dataType: 'json',
         url: "user/search",
         data: {query:query},
         success: function(data) {
-            $('#user_table').html(data);
+            fillData(time1, data);
         }
     });
+}
+function fillData(time, data) {
+    console.log('   time:  ' + time);
+    if (lastTime <= time) {
+        lastTime = time;
+        $('#user_table').html(data);
+    }
 }
 
 function searchFilm()
