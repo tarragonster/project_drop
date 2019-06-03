422669df3f802158445d4685c018b070bf4700c5
Cuong Do <cuong.do@acaziasoft.com>
2019-06-03 14:48:52 +0700

    Add push notification

--- application/config/push.php
+++ application/config/push.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['gcm_api_key'] = 'AIzaSyCSj1mK1ZNHzJer35H8iDB8vN0ZSxhY-Og';
+$config['gcm_api_key'] = 'AAAAGkvpT8c:APA91bEAaiPa7hoTyVyioHLEfdJRW1Ac13z6CF9vFz6C1HNo7eGBFtprxuujMimgLlCzHYz52-E1dC2w1VcD6umCMEGqYtkOBJHSiYB-llH5i8aNy0-EiMplvn09AOagvLJpjNoiYGyM'; // 112942731207
 $config['push_table'] = 'notification_queue';
 
 $config['apns_server'] = 'gateway.push.apple.com:2195';
