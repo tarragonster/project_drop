9493ae118a9413a4fd929b30fca200703bb4915f
tarragonster <tug.thanh@gmail.com>
2019-07-28 14:10:53 +0700

    Finish styling for sidebar menu pending settings and locgout

--- application/views/admin_main_layout.php
+++ application/views/admin_main_layout.php
@@ -121,10 +121,22 @@
 			<div id="sidebar-menu">
 				<ul>
 					<li>
-						<a href="<?php echo base_url(''); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 1 ? ' active' : ''; ?>"><i class="fa fa-tachometer-alt"></i> <span>Dashboard</span></a>
+						<a href="<?php echo base_url(''); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 1 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 1) { ?>
+                                <img src= "assets/images/left-sidebar/Dashboard-active.svg" alt="Dashboard">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/dashboard.svg" alt="Dashboard">
+                            <?php } ?>
+                            <span>Dashboard</span></a>
 					</li>
 					<li class="has_sub">
-						<a href="#" class="waves-effect<?php echo isset($parent_id) && $parent_id == 2 ? ' active' : ''; ?>"> <i class="fa fa-users"></i> <span>Users</span> </a>
+						<a href="#" class="waves-effect<?php echo isset($parent_id) && $parent_id == 2 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 2) { ?>
+                                <img src= "assets/images/left-sidebar/User-active.svg" alt="Users">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/User.svg" alt="User">
+                            <?php } ?>
+                            <span>Users</span> </a>
 						<ul class="list-unstyled">
 							<li <?php echo($sub_id == 21 ? 'class="active"' : ''); ?>>
 								<a href="<?php echo base_url('user'); ?>"><span>Active Users</span></a>
@@ -142,18 +154,40 @@
 						</ul>
 					</li>
 					<li>
-						<a href="<?php echo base_url('product'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 3 ? ' active' : ''; ?>"><i class="fa fa-leaf"></i> <span>Stories</span></a>
+						<a href="<?php echo base_url('product'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 3 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 3) { ?>
+                                <img src= "assets/images/left-sidebar/stories-active.svg" alt="Dashboard">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/stories.svg" alt="Stories">
+                            <?php } ?>
+                            <span>Stories</span></a>
 					</li>
 					<li>
-						<a href="<?php echo base_url('preview'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 10 ? ' active' : ''; ?>"><i class="fa fa-industry"></i>
-							<span>Explore</span></a>
+						<a href="<?php echo base_url('preview'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 10 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 10) { ?>
+                                <img src= "assets/images/left-sidebar/Explore-active.svg" alt="Explore">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/Explore.svg" alt="Explore">
+                            <?php } ?>
+                            <span>Explore</span></a>
 					</li>
 					<li>
-						<a href="<?php echo base_url('collection'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 4 ? ' active' : ''; ?>"><i class="fa fa-industry"></i>
-							<span>Collections</span></a>
+						<a href="<?php echo base_url('collection'); ?>" class="waves-effect<?php echo isset($parent_id) && $parent_id == 4 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 4) { ?>
+                                <img src= "assets/images/left-sidebar/Collections-active.svg" alt="Collections">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/Collections.svg" alt="Collections">
+                            <?php } ?>
+                            <span>Collections</span></a>
 					</li>
 					<li class="has_sub">
-						<a href="#" class="waves-effect<?php echo isset($parent_id) && $parent_id == 9 ? ' active' : ''; ?>"> <i class="ti-comment"></i> <span>Comments</span> </a>
+						<a href="#" class="waves-effect<?php echo isset($parent_id) && $parent_id == 9 ? ' active' : ''; ?>">
+                            <?php if (isset($parent_id) && $parent_id == 9) { ?>
+                                <img src= "assets/images/left-sidebar/Comments-active.svg" alt="Comments">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/Comments.svg" alt="Comments">
+                            <?php } ?>
+                            <span>Comments</span> </a>
 						<ul class="list-unstyled">
 							<li <?php echo($sub_id == 91 ? 'class="active"' : ''); ?>>
 								<a href="<?php echo base_url('comment'); ?>"><span>Comments</span></a>
@@ -164,11 +198,22 @@
 						</ul>
 					</li>
                     <li>
-                        <a href="<?php echo base_url('lockscreen'); ?>" class="waves-effect"><i class="fa fa-industry"></i>
+                        <a href="<?php echo base_url('lockscreen'); ?>" class="waves-effect">
+                            <?php if (isset($parent_id) && $parent_id == 1) { ?>
+                                <img src= "assets/images/left-sidebar/Settings-active.svg" alt="Settings">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/Settings.svg" alt="Settings">
+                            <?php } ?>
+
                             <span>Settings</span></a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url('logout'); ?>" class="waves-effect"><i class="fa fa-industry"></i>
+                        <a href="<?php echo base_url('logout'); ?>" class="waves-effect">
+                            <?php if (isset($parent_id) && $parent_id == 1) { ?>
+                                <img src= "assets/images/left-sidebar/Logout-active.svg" alt="Logout">
+                            <?php }else{ ?>
+                                <img src= "assets/images/left-sidebar/Logout.svg" alt="Logout">
+                            <?php } ?>
                             <span>Log Out</span></a>
                     </li>
 				</ul>
