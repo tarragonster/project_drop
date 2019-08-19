157d64f72a11dcad349d92fb07dfe7b9d794d785
Cuong Do <cuong.do@acaziasoft.com>
2019-08-19 12:34:22 +0700

    Remove font

--- application/views/admin_main_layout.php
+++ application/views/admin_main_layout.php
@@ -10,7 +10,6 @@
 
 	<title>10 Block cPanel</title>
 
-    <link href="" rel="stylesheet">
 	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css"/>
 	<link href="<?php echo base_url('assets/css/core.css'); ?>" rel="stylesheet" type="text/css"/>
 	<link href="<?php echo base_url('assets/css/components.css'); ?>" rel="stylesheet" type="text/css"/>
