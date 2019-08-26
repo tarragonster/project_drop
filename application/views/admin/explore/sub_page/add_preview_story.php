a486c0fac4e3fa58031b16c350628117fe4a84b4
DuyenNguyen <duyennth@acaziasoft.com>
2019-08-26 14:35:04 +0700

    Explore - Add preview story

--- application/views/admin/explore/sub_page/add_preview_story.php
+++ application/views/admin/explore/sub_page/add_preview_story.php
@@ -18,17 +18,52 @@
 	        <input type="text" name="product_id" id="story_key" required class="form-control custom-input" onkeyup='searchStory()' placeholder="Search Story Name"/>
 	        <div id="other_story"></div>
 	    </div>
-	    <div class="form-group" style="padding-right: 0;">
+	    <div class="form-group" style="padding-right: 0;margin-top: 50px;">
             <label>Explore Preview Image</label>
             <div class="row" style="padding-left: 10px">
-                <img id='explore_image' width="140" height="150" src="<?php echo (!empty($product['explore_img'])) ? base_url($product['explore_img']) : base_url('assets/images/borders/650x688@3x.png')?>"/>
+                <img id='explore_image' width="140" height="150" src="<?php echo base_url('assets/images/borders/650x688@3x.png')?>"/>
                 <div class='err-format' id="ex_err1">Image format is not suppported</div>
                 <div class='err-size' id="ex_err2">The size must be less than 1MB</div>
-                <div class="uploader" onclick="$('#exploreImg').click()">
-                    <button type="button" class="btn  ">Upload</button>
+                <div class="uploader" onclick="$('#exploreImg').click()" style="display: none">
+                    <button type="button" class="btn">Upload alternative image</button>
                     <input type="file" accept="image/*" name="explore_img" id="exploreImg" class="imagePhoto"/>
                 </div>
             </div>
         </div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	var exploreImgLoader = document.getElementById('exploreImg');
+	if (exploreImgLoader) {
+	    exploreImgLoader.addEventListener('change', handleExploreImage, false);
+	}
+	function handleExploreImage(e) {
+	    const arr = ['jpg', 'png', 'jpeg', 'gif', 'pdf', 'JPG', 'PNG', 'JPEG', 'GIF', 'PDF']
+
+	    var explore_image = $('#exploreImg').val()
+	    var fileSize = document.getElementById('exploreImg').files[0].size;
+	    isImage = arr.includes(explore_image.split('.').pop())
+
+	    if (isImage == false) {
+	        $('#ex_err1').css('display', 'block');
+	        $('#ex_err2').css('display', 'none');
+	        $('#explore_image').attr('src', BASE_APP_URL + 'assets/images/borders/650x688@3x.png');
+	    } 
+	    else if(fileSize /(1024*1024) > 1){
+	        $('#ex_err2').css('display', 'block');
+	        $('#ex_err1').css('display', 'none');
+	        $('#explore_image').attr('src', BASE_APP_URL + 'assets/images/borders/650x688@3x.png');
+	    }else {
+	        $('#ex_err1').css('display', 'none');
+	        $('#ex_err2').css('display', 'none');
+	        var reader = new FileReader();
+	        var reader = new FileReader();
+	        reader.onload = function (event) {
+
+	            $('#explore_image').attr('src',event.target.result);
+	        }
+	        reader.readAsDataURL(e.target.files[0]);
+	    }
+	}
+</script>
